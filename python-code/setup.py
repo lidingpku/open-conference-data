d57b4b149668877737d6700c1253fdd932cb9d96
lidingpku <dingli@ubuntu.ubuntu-domain>
2013-06-12 07:39:35 -0700

    created skeleton project following http://learnpythonthehardway.org/book/ex46.html. package name libconfdata
    	new file:   .gitignore
    	new file:   python-code/README
    	new file:   python-code/libconfdata/__init__.py
    	new file:   python-code/setup.py
    	new file:   python-code/tests/__init__.py
    	new file:   python-code/tests/libconfdata_tests.py

---
+++ python-code/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A project for create, open, refine, and reuse conference data.',
+    'author': 'Li Ding',
+    'url': 'https://github.com/lidingpku/open-conference-data',
+    'download_url': 'https://github.com/lidingpku/open-conference-data',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['libconfdata'],
+    'scripts': [],
+    'name': 'Open Conference Data'
+}
+
+setup(**config)
